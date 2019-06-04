049034779cb8a4757bd6d940ad47ec38bbe7be4a
Macaulish64 <justfreefood@163.com>
2019-06-04 19:43:36 +0800

    update readme
    update readme

--- myqpp/controller/customer.js
+++ myqpp/controller/customer.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var acService = require(global.rname+'/service/acService');
+//var acService = require(global.rname+'/service/acService');
 
 /* GET users listing. */
 router.post('/setInitTemp', function(req, res, next) {
   var roomId=req.body.roomID;
   var initTemp=req.body.initTemp;
-  return '{ "status":"'+acService.setInitTemp(roomId,initTemp)+'"}';
+ // return '{ "status":"'+acService.setInitTemp(roomId,initTemp)+'"}';
 });
 
 module.exports = router;
